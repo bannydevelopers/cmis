9a6ade758757a0b1d587e3cbfede70e112ee6c1c
Wizdon <software@banny.co.tz>
2023-04-18 15:15:25 +0300

    Staff module, staff listing

--- cmis/modules/HR/staff.php
+++ cmis/modules/HR/staff.php
@@ -1,6 +1,11 @@
 <?php 
 $db = db::get_connection(storage::init()->system_config->database);
-$designation = $db->select('designation','designation_id,designation_id')->fetchALL();
+$designation = $db->select('designation','designation_id,designation_name')->fetchALL();
 
-$staff = $db->select('staff')->fetchAll();
+$staff = $db->select('staff')
+            ->join('user','user_id=user_reference')
+            ->join('user as creator', 'creator.user_id=user.created_by','LEFT OUTER')
+            ->join('designation', 'designation_id=designation','LEFT OUTER')
+            ->join('bank', 'bank.bank_id=staff.bank_id','LEFT OUTER')
+            ->fetchAll();
 echo helper::find_template('Staff', ['designation'=>$designation, 'staff'=>$staff]);
