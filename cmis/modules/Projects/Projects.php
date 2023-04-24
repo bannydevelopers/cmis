a009e869d23be7a23c394fa1ddf54ab3dfd0fa36
abigairi <abigailjonathan150@gmail.com>
2023-04-24 13:11:30 +0300

    store

--- cmis/modules/Projects/Projects.php
+++ cmis/modules/Projects/Projects.php
@@ -1,3 +1,30 @@
 <?php 
+$db = db::get_connection(storage::init()->system_config->database);
 
-echo helper::find_template('Projects', []);
+//var_dump($_POST);
+if(isset($_POST['project_name'])){
+    $data = [
+        'project_name'=>addslashes($_POST['project_name']),
+        'started_date'=>addslashes($_POST['started_date']),
+        'end_date'=>addslashes($_POST['end_date']),
+        'project_discription'=>addslashes($_POST['project_deiscription']),
+        'project_burget'=>addslashes($_POST['project_burget']),
+        'project_manager'=>addslashes($_POST['project_manager']),
+        ];
+   
+$user = $db->select('user','user_id,user_name')->fetchALL();
+$store = $db->select('store','store_id,store_name')->fetchALL();
+$staff = $db->select('staff')
+            ->join('user','user.user_id=staff.user_reference')
+            ->join('user', 'user_id=user','LEFT')
+            ->fetchAll();
+
+$k = $db->insert('project', $data);
+if(intval($k)) $msg = 'project added successful';
+    else  $msg = 'project add failed';
+}
+$project = $db->select('project','project_id,name')->fetchALL();
+        
+    //var_dump('<pre>',$project);
+    echo helper::find_template('projects', ['project'=>$project, 'db'=>$db]);
+        
--- cmis/modules/Store/Stores.php
+++ cmis/modules/Store/Stores.php
@@ -1,13 +1,14 @@
 <?php 
 $db = db::get_connection(storage::init()->system_config->database);
 
-    //var_dump($_REQUEST);
+//var_dump($_POST);
 if(isset($_POST['store_name'])){
     $data = [
         'store_name'=>addslashes($_POST['store_name']),
         'store_location'=>addslashes($_POST['store_location'])
     ];
     $k = $db->insert('store', $data);
+//var_dump($db->error());
     if(intval($k)) $msg = 'store added successful';
     else  $msg = 'store add failed';
 }
