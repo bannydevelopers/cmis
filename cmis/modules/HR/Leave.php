0508a4b30af59d53becde437935fd5a9bfea6505
Jasper <software@banny.co.tz>
2023-04-28 12:51:24 +0300

    l

--- cmis/modules/HR/Leave.php
+++ cmis/modules/HR/Leave.php
@@ -8,12 +8,12 @@ $msg = '';
 if(isset($_POST['leave_type'])){
     $data = [
         'staff_id'=>$me['staff_id'], 
-        'leave_description'=>addslashes($_POST['leave_description']), 
+        'leave_application_description'=>addslashes($_POST['leave_description']), 
         'leave_start_date'=>helper::format_time($_POST['leave_start'], 'Y-m-d H:i:s'), 
-        'leave_type'=>intval($_POST['leave_type']), 
+        'leave_application_type'=>intval($_POST['leave_type']), 
         'leave_length'=>intval($_POST['leave_length']), 
         'application_date'=>date('Y-m-d H:i:s'), 
-        'responsibility_assignee'=>intval($_POST['responsility_assignee'])
+        'responsibility_assignee'=>intval($_POST['responsibility_assignee'])
     ];
     if(!intval($data['responsibility_assignee'])) $data['assignee_response_date'] = date('Y-m-d H:i:s');
     $k = $db->insert('leave_application', $data);
