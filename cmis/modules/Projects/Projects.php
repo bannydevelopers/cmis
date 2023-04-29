9cf16d64d06776ec567584bdc4cf577333b36f87
Wizdon <software@banny.co.tz>
2023-04-29 11:50:27 +0300

    Edited projects

--- cmis/modules/Projects/Projects.php
+++ cmis/modules/Projects/Projects.php
@@ -1,30 +1,37 @@
 <?php 
 $db = db::get_connection(storage::init()->system_config->database);
-
+$my = helper::init()->get_session_user();
 //var_dump($_POST);
 if(isset($_POST['project_name'])){
-    $data = [
-        'project_name'=>addslashes($_POST['project_name']),
-        'started_date'=>addslashes($_POST['started_date']),
-        'end_date'=>addslashes($_POST['end_date']),
-        'project_discription'=>addslashes($_POST['project_deiscription']),
-        'project_burget'=>addslashes($_POST['project_burget']),
-        'project_manager'=>addslashes($_POST['project_manager']),
+    if($helper::init()->user_can('can_add_project')){
+        $staff = $db->select('staff','staff_id')
+                    ->where(['user_reference'=>$my['user_id']])
+                    ->fetch();
+        $data = [
+            'project_name'=>addslashes($_POST['project_name']), 
+            'project_starting_date'=>helper::format_time($_POST['project_starting_date'],'Y-m-d H:i:s'), 
+            'project_ending_date'=>helper::format_time($_POST['project_ending_date'],'Y-m-d H:i:s'), 
+            'project_description'=>addslashes($_POST['project_description']), 
+            'project_burget'=>addslashes($_POST['project_burget']), 
+            'staff_id'=>$staff['staff_id'], 
+            'created_time'=>date('Y-m-d H:i:s')
         ];
-        //var_dump($db->error());
-$user = $db->select('user','user_id,user_name')->fetchALL();
-$store = $db->select('store','store_id,store_name')->fetchALL();
-$staff = $db->select('staff')
-            ->join('user','user.user_id=staff.user_reference')
-            ->join('user', 'user_id=user','LEFT')
-            ->fetchAll();
-
-$k = $db->insert('project', $data);
-if(intval($k)) $msg = 'project added successful';
-    else  $msg = 'project add failed';
+        $k = $db->insert('project', $data);
+        if(!$db->error() && $k){
+            $msg = 'Project created successful';
+        }
+        else{
+            $msg = 'Project create error';
+        }
+    }
+    else $msg = 'Permission denied';
 }
-$project = $db->select('project','project_id,name')->fetchALL();
+$project = $db->select('project', 'project.*,user.first_name,user.middle_name,user.last_name')
+              ->join('staff','staff.staff_id=project.staff_id')
+              ->join('user','staff.user_reference=user.user_id')
+              ->fetchALL();
         
-    //var_dump('<pre>',$project);
-    echo helper::find_template('projects', ['project'=>$project, 'db'=>$db]);
+//var_dump('<pre>',$project);
+$data = ['project'=>$project];
+echo helper::find_template('projects', ['project'=>$project]);
         
