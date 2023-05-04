16bac937c79c9580c53603816e9d6a8f9855cc45
abigairi <abigailjonathan150@gmail.com>
2023-05-04 10:41:18 +0300

    project database

--- cmis/modules/Projects/Projects.php
+++ cmis/modules/Projects/Projects.php
@@ -1,9 +1,10 @@
 <?php 
 
 $db = db::get_connection(storage::init()->system_config->database);
+$my = helper::init()->get_session_user();
 $ok = false;
+$me = $db->select('staff')->where(['user_reference'=>$my['user_id']])->fetch();
 $msg = '';
-
 
 $request = $_SERVER['REQUEST_URI'];
 if(isset($_POST['project_name'])){
@@ -13,7 +14,7 @@ if(isset($_POST['project_name'])){
         'project_ending_date'=>$_POST['project_ending_date'],
         'project_burget'=>$_POST['project_burget'],
         'project_description'=>$_POST['project_description'],
-        'staff_id'=>$_POST['staff_id'],
+        'staff_id'=>$me['staff_id'],
         'created_time'=>date('Y-m-d H:i:s')
     ];
     $k = $db->insert('project', $data);
@@ -24,11 +25,10 @@ if(isset($_POST['project_name'])){
         $ok =true;
     }
     else $msg = 'Error adding project';
-    var_dump($db->error());
+    //var_dump($db->error());
 }
  $staff= $db->select('staff','staff_id,staff_name')
                   ->fetchALL();
-
 
 $project = $db->select('project')->order_by('project_id', 'desc')->fetchAll();
 $data = ['project'=>$project,'msg'=>$msg, 'status'=>$ok,'request_uri'=>$request];
