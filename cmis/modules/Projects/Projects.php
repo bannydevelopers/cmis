a075c8bf2c50ad976bd8477abcc9806e36758613
abigairi <abigailjonathan150@gmail.com>
2023-05-08 15:08:06 +0300

    invoice and supplier

--- cmis/modules/Projects/Projects.php
+++ cmis/modules/Projects/Projects.php
@@ -9,14 +9,6 @@ $msg = '';
 $request = $_SERVER['REQUEST_URI'];
 $storage = storage::init();
 
-$file = __DIR__.'/module.json';
-$json = file_get_contents($file);
-$mod_conf = json_decode($json, false);
-
-if(isset($_POST['project_manager_role'])){
-    $mod_conf->pm_manager = intval($_POST['project_manager_role']);
-    file_put_contents($file, json_encode($mod_conf, JSON_PRETTY_PRINT));
-}
 if(isset($_POST['project_name'])){
     $data = [
         'project_name'=>$_POST['project_name'], 
@@ -39,39 +31,18 @@ if(isset($_POST['project_name'])){
     //var_dump($db->error());
 }
 if(isset($storage->request[3]) && intval($storage->request[3])){
-    $cols = "project.*,designation.designation_name, concat( first_name,' ',last_name) as pm_name";
-    $project = $db->select('project', $cols)
-                 ->join('user', 'project_manager=user_id')
-                 ->join('staff','staff.user_reference=user.user_id')
-                 ->join('designation', 'designation_id=staff.designation')
-                 ->where(['project_id'=>$storage->request[3]])
-                 ->order_by('project_id', 'desc')->fetch();
-    var_dump($db->error());
+    $project = $db->select('project')->where(['project_id'=>$storage->request[3]])->fetch();
     $activities = $db->select('activities')->where(['activity_id'=>$storage->request[3]])->fetchAll();
     $data = [
         'project'=>$project,
-        'activity'=>$activities,
-        'currency'=>$storage->system_config->system_currency
+        'activity'=>$activities
     ];
     die(helper::find_template('project_details', $data));
 }
-
-$staff = $db->select('user',"user_id,concat(first_name,' ', last_name) as pm_name")
-            ->join('staff', 'user_reference=user_id')
-            ->where(['system_role'=>$mod_conf->pm_manager])
-            ->fetchALL();
+$staff= $db->select('user',"user_id,concat(first_name,' ', last_name) as pm_name")->fetchALL();
 
 $project = $db->select('project', "project.*, concat( first_name,' ',last_name) as pm_name")
               ->join('user', 'project_manager=user_id')
               ->order_by('project_id', 'desc')->fetchAll();
-$roles = $db->select('role')->fetchAll();
-$data = [
-    'project'=>$project,
-    'msg'=>$msg, 
-    'status'=>$ok,
-    'request_uri'=>$request,
-    'user'=>$staff,
-    'currency'=>$storage->system_config->system_currency,
-    'roles'=>$roles
-];
+$data = ['project'=>$project,'msg'=>$msg, 'status'=>$ok,'request_uri'=>$request,'user'=>$staff];
 echo helper::find_template('projects', $data);
