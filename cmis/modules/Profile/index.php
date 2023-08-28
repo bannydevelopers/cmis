17a687773b8a4ed48cefe90c7059b05cbb8725d8
Baraka Mghumba <wizdontz@gmail.com>
2023-08-28 19:08:55 +0300

    ....

--- cmis/modules/Profile/index.php
+++ cmis/modules/Profile/index.php
@@ -1,6 +1,18 @@
 <?php 
 
 $db = db::get_connection(storage::init()->system_config->database);
+if(isset($_POST['new_pass'])){
+    $newpass = helper::create_hash($_POST['new_pass']);
+    $oldpass = helper::create_hash($_POST['old_pass']);
+    $uid = $_POST['user'];
+    $me = $db->select('user', 'password')->where(['user_id'=>$uid])->limit(1)->fetch();
+    if($oldpass == $me['password']){
+        $db->update('user', ['password'=>$newpass])->where(['user_id'=>$uid])->commit();
+        if(!$db->error()) die('ok');
+        else die($db->error()['message']);
+    }
+    die('Old password does not match');
+}
 $user = $db->select('staff')
            ->join('user','user_id=user_reference')
            ->where(['user_id'=>helper::init()->get_session_user('user_id')])
