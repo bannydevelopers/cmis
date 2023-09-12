b959ee6d46ae7850053ac6d929dca56e1612439d
Baraka Mghumba <wizdontz@gmail.com>
2023-09-12 13:41:02 +0300

    changed phpmailer sender account to smatp2go

--- cmis/modules/Sales/Reports.php
+++ cmis/modules/Sales/Reports.php
@@ -1,29 +1,39 @@
 <?php 
 
 $db = db::get_connection(storage::init()->system_config->database);
-$ok = false;
-$msg = '';
 
 $request = $_SERVER['REQUEST_URI'];
-if(isset($_POST['product_name'])){
-    $data = [
-        'product_name'=>$_POST['product_name'], 
-        'product_description'=>$_POST['product_description'], 
-        'product_unit'=>$_POST['product_unit'], 
-        'created_time'=>date('Y-m-d H:i:s')
-        
-    ];
-    //var_dump($db->error());
-    $k = $db->insert('product', $data);
-   
-    if(!$db->error() && $k) {
-        $msg = 'product added successful';
-        $ok =true;
-    }
-    else $msg = 'Error adding product';
-    //var_dump($db->error());
+$data = ['msg'=>'', 'status'=>false,'request_uri'=>$request];
+
+$supported = ['customers', 'sales', 'products'];
+if(isset(storage::init()->request[3])){
+    $req = storage::init()->request[3];
+
+    $template = '404';
+    if(in_array(storage::init()->request[3], $supported)){
+        $template = "{$req}_report";
+        if($req == 'customers'){
+            $customers = $db->select('invoice', 'customer.*, COUNT(invoice_id) as frequence')
+                        ->join('customer', 'customer.customer_id=invoice.customer')
+                        ->where(['invoice_type'=>'tax'])
+                        ->group_by('customer_id')
+                        ->order_by('frequence', 'DESC')
+                        //->limit(30)
+                        ->fetchAll();
+
+            $data['customers'] = $customers;
+        }
+    } 
 }
-$msg = 'Reports are generating...';
-$product = $db->select('product')->order_by('product_id', 'desc')->fetchAll();
-$data = ['product'=>$product,'msg'=>$msg, 'status'=>$ok,'request_uri'=>$request];
-echo helper::find_template('reports', $data);
+else{
+    $customers = $db->select('invoice', 'customer.customer_name, COUNT(invoice_id) as frequence')
+                ->join('customer', 'customer.customer_id=invoice.customer')
+                ->where(['invoice_type'=>'tax'])
+                ->group_by('customer_id')
+                ->order_by('frequence', 'DESC')
+                ->limit(10)
+                ->fetchAll();
+    $data['customers'] = $customers;
+    $template = 'reports';
+}
+echo helper::find_template($template, $data);
